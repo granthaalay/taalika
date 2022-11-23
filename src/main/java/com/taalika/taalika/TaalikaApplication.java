99a93d6483ff597759d72ffa3bbb0ec959aa783b
Saptarishi Das <sdas@reliaquest.com>
2022-11-24 00:13:48 +0530

    project init

---
+++ src/main/java/com/taalika/taalika/TaalikaApplication.java
@@ -0,0 +1,13 @@
+package com.taalika.taalika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaalikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaalikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taalika/taalika/TaalikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taalika.taalika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaalikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
